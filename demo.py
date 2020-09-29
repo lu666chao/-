3f5d57db84a55b8109da2479d3a2c31cefd7fafc
lu666chao <70931861+lu666chao@users.noreply.github.com>
2020-09-29 08:24:41 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,9 +3,9 @@ filename = 'demo.py'
 with open(filename,'r',encoding='utf-8') as fp:
      lines = fp.readlines()          #读取所有行
   
-  #获取最长的行的长度
-     maxLength = max(map(len,lines))
-     print(maxLength)
+  
+maxLength = max(map(len,lines)) #获取最长的行的长度
+print(maxLength)
   #遍历所有行
 for index,line in enumerate(lines):
      newLine = line.rstrip()            #删除每行右侧的空白字符
