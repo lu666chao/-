22711f7060903a6b20316d0db7a4fb7ba0a9cec9
lu666chao <70931861+lu666chao@users.noreply.github.com>
2020-10-27 08:49:11 +0800

    Add files via upload

---
+++ 点名葫芦娃.py
@@ -0,0 +1,68 @@
+import tkinter
+import tkinter.messagebox
+import random
+import threading
+import itertools
+import time
+import pygame
+
+file=r"D:\CloudMusic\王迟 - 葫芦金刚.mp3"
+def closeWindow():
+    root.flag = False
+    time.sleep(0.1)  # 延迟0.1秒执行
+    root.destroy()
+
+
+def clickStart():
+    t = threading.Thread(target=shuffleUsers)
+    t.start()
+    btnStart['state'] = 'disabled'
+    btnStop['state'] = 'normal'
+    pygame.mixer.init()
+    pygame.mixer.music.load(file)
+    pygame.mixer.music.play(1, 5)
+
+
+def clickStop():
+    global times
+    root.flag = False  # 暂停线程
+    pygame.mixer.music.stop()
+    time.sleep(0.1)
+    times += 1
+    tkinter.messagebox.showinfo(title="Congratulations!", message=str(times) + ": " + showLabel['text'])
+    btnStart['state'] = 'normal'
+    btnStop['state'] = 'disabled'
+
+
+def shuffleUsers():  # 开始洗牌线程
+    root.flag = True
+    t = users[:]
+    random.shuffle(t)
+    t = itertools.cycle(t)
+    while root.flag:
+        showLabel['text'] = next(t)
+        time.sleep(0.1)
+
+
+root = tkinter.Tk()
+root.title("Name Shuffle")
+root.geometry("400x400+480+200")
+root.resizable(False, False)
+
+root.flag = False  # 线程状态
+times = 0
+users = ['周嘉铖','钱珑超','徐展','尤桉哲','钱涛','黄舒怡','胡志辉','吴昭耀','陈萌萌','郑巧悦','陈艳','梁明皓','蒋俊超','徐颖','倪宏涛','潘梦倩','王中阳','毛贞强','张嫒','朱速航','陈涛','陆元超','叶振雄','奚申杰','叶梦婷','徐丽丽','潘艳']
+
+btnStart = tkinter.Button(root, text='Start', command=clickStart)
+btnStart.place(x=30, y=10, width=80, height=20)
+
+btnStop = tkinter.Button(root, text='Stop', command=clickStop)
+btnStop['state'] = 'disabled'
+btnStop.place(x=300, y=10, width=80, height=20)
+
+showLabel = tkinter.Label(root, text='')
+showLabel['fg'] = 'red'
+showLabel.place(x=150, y=180, width=100, height=20)
+
+root.protocol("WM_DELETE_WINDOW", closeWindow)
+root.mainloop()
